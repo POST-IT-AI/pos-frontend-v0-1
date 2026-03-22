47329bc6a8d025d5dc8593ec0d5882639cdee346
Rabab <rabab.radoslaw@gmail.com>
2026-03-22 22:42:08 +0700

    refactor: fix react-refresh ESLint warnings by separating variant exports

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button-variants";
 import { useTranslation } from "react-i18next";
 
 export function Component() {
